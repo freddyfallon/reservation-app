95c630430b5c0e89f39f945b7a0815baa0f3af7c
Freddy Fallon <freddyfallon@gmail.com>
2018-02-27 12:31:54 +0000

    🔧 Add Swagger to describe endpoints

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 const db = require('./config/db');
 const router = require('./src/routes/index');
 const app = express();
 
 const port = 5000;
 
-
 app.use(bodyParser.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) {
