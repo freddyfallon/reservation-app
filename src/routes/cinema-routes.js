81cd7d3148da5faf93dc85622bc33cd43d6b469b
Freddy Fallon <freddyfallon@gmail.com>
2018-02-27 08:56:58 +0000

    ✨ Add route to lookup cinema by id

--- src/routes/cinema-routes.js
+++ src/routes/cinema-routes.js
@@ -1,11 +1,27 @@
+const ObjectID = require('mongodb').ObjectID;
+
 const cinemas = (app, db) => {
+  app.get('/cinemas/:id', (req, res) => {
+    const id = req.params.id;
+    const details = {'_id': new ObjectID(id)};
+    db.collection('cinemas').findOne(details, (err, item) => {
+      if (err) {
+        console.error(err);
+        res.send({'error': 'An error has occured'})
+      } else {
+        console.log('🍍', item)
+        res.send(item);
+    }
+    });
+  });
+
   app.post('/cinemas', (req, res) => {
     const cinema = { name: req.body.name, description: req.body.description };
     db.collection('cinemas').insert(cinema, (err, result) => {
       if (err) { 
         res.send({ 'error': 'An error has occurred' }); 
       } else {
-        res.send(result.ops[0]);
+        res.send(`${result.ops[0].name} successfully added`);
       }
     });
   });
